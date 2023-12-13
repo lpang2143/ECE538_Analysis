732d466f86606597d8647cbbb8bb0f515647d60a
Meet Patel <mvpatel@princeton.edu>
2023-12-12 21:31:08 -0500

    analysis

--- analysisDNS.py
+++ analysisDNS.py
@@ -1,5 +1,5 @@
 from enum import Enum
-import tldextract
+import tldextract as tld
 from resolveSOA import getSOA
 import sys
 
@@ -19,29 +19,37 @@ def main():
         ns = line[1].split(' ')[1:]
         if 'NXDOMAIN' in ns or 'SERVFAIL' in ns or not ns:
             continue
+        
+        # things we need:
+        # -tld(w)
+        # -tld(ns)
+        # -isHTTPS(w)
+        # -SAN(w)
+        # -SOA(w)
+        # -SOA(ns)
+        # -concentration(ns)
 
-        # check if tld matches
-        if ns_type == NS_TYPE.UNKNOWN:
-            w_ext = tldextract.extract(w)
-            w_tld = ".".join([w_ext.domain, w_ext.suffix])
-            for n in ns:
-                n_ext = tldextract.extract(n)
-                n_tld = ".".join([n_ext.domain, n_ext.suffix])
-                host = n_tld
-                if (w_tld == n_tld):
-                    ns_type = NS_TYPE.PRIVATE
-                    break;
+        w_ext = tld.extract(w)
+        tld_w = ".".join([w_ext.domain, w_ext.suffix])
+        tld_ns = [".".join([tld.extract(n).domain+tld.extract(n).suffix]) for n in ns]
+        isHTTPS_w = isHTTPS(w)
+        SAN_w = getSAN(w)
+        SOA_w = getSOA(w)
+        SOA_ns = [getSOA(n) for n in ns]
+        conc_ns = [getConcentration(n) for n in ns]
                 
-
-        # check if SOA matches
-        if ns_type == NS_TYPE.UNKNOWN:
-            w_soa = getSOA(w)
-            for n in ns:
-                n_soa = getSOA(n)
-                if (w_soa != n_soa):
-                    ns_type = NS_TYPE.THIRD
-                    break;
-
+        # check if tld matches
+        if tld_w in tld_ns:
+            ns_type = NS_TYPE.PRIVATE
+        # check if HTTPS and tld(ns) in SAN(w)
+        elif (isHTTPS_w and any(n in SAN_w for n in tld_ns)):
+            ns_type = NS_TYPE.PRIVATE
+        # check if SOA(ns) != SOA(w)
+        elif (SOA_ns != SOA_w):
+            ns_type = NS_TYPE.THIRD
+        # check if concentration(ns) >= 50
+        elif (conc_ns >= 50):
+            ns_type = NS_TYPE.THIRD
         
         if ns_type == NS_TYPE.UNKNOWN:
             final = 'UNKNOWN'
