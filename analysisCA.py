e11ef8e5faa660ea6156bda0bbb1fc73a623650c
Meet Patel <mvpatel@princeton.edu>
2023-12-12 21:34:27 -0500

    analysis

---
+++ analysisCA.py
@@ -0,0 +1,60 @@
+from enum import Enum
+import tldextract as tld
+from resolveSOA import getSOA
+import sys
+
+class CA_TYPE(Enum):
+    UNKNOWN = 1
+    PRIVATE = 2
+    THIRD = 3
+
+def main():
+    f = open('ns_unique', 'r')
+    out = open(sys.argv[1], 'w')
+    for line in f:
+        ca_type = CA_TYPE.UNKNOWN
+        host = ''
+        line = line.split(':')
+        w = line[0]
+        ns = line[1].split(' ')[1:]
+        if 'NXDOMAIN' in ns or 'SERVFAIL' in ns or not ns:
+            continue
+        
+        # things we need:
+        # -tld(w)
+        # -tld(ca_url)
+        # -isHTTPS(w)
+        # -SAN(w)
+        # -SOA(w)
+        # -SOA(ca_url)
+
+        w_ext = tld.extract(w)
+        tld_w = ".".join([w_ext.domain, w_ext.suffix])
+        ca = getCA(w)
+        ca_ext = tld.extract(ca)
+        tld_ca = ".".join([ca_ext.domain, ca_ext.suffix])
+        isHTTPS_w = isHTTPS(w)
+        SAN_w = getSAN(w)
+        SOA_w = getSOA(w)
+        SOA_ca = getSOA(ca)
+
+        # check if tld matches
+        if tld_w == tld_ca:
+            ca_type = CA_TYPE.PRIVATE
+        elif (isHTTPS_w and tld_ca in SAN_w):
+            ca_type = CA_TYPE.PRIVATE
+        elif (SOA_ca != SOA_w):
+            ca_type = CA_TYPE.THIRD
+
+        
+        if ca_type == CA_TYPE.UNKNOWN:
+            final = 'UNKNOWN'
+        elif ca_type == CA_TYPE.PRIVATE:
+            final = 'PRIVATE'
+        else:
+            final = 'THIRD'
+
+        out.write(f"{w} {final} {host}\n")
+    f.close()
+    out.close()
+        
