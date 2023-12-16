4bbe0e00c44bde0b5b3ba4c2a6caeaf24f88663e
Student Worker <studentworker@dynamic-oit-vapornet100-10-9-182-222.princeton.edu>
2023-12-15 19:22:37 -0500

    CA analysis

--- analysisCA.py
+++ analysisCA.py
@@ -4,30 +4,37 @@ from resolveSOA import getSOA
 import sys
 from utils import DEP_TYPE
 
-ca_map = {'Amazon': 'https://www.amazontrust.com', 'Google Trust Services LLC': 'https://pki.goog/', }
+ca_map = {'Amazon': 'amazontrust.com', 'Google Trust Services LLC': 'pki.goog', 'Entrust, Inc.': 'entrust.com', 
+            'DigiCert Inc': 'digicert.com', 'Sectigo Limited': 'sectigo.com', 'COMODO CA Limited': 'comodoca.com', 'Microsoft Corporation': 'microsoft.com',
+            'Let\'s Encrypt': 'letsencrypt.org', 'Cloudflare, Inc.': 'cloudflare.com', 'GlobalSign nv-sa': 'globalsign.com', 'ZeroSSL': 'zerossl.com'}
 
 def main():
-    # f = open('certificates.txt', 'r')
-    f = ["aan.amazon.com/Issuer: ((('countryName', 'US'),), (('organizationName', 'Amazon'),), (('commonName', 'Amazon RSA 2048 M01'),))/Subject: ((('commonName', 'aan.amazon.com'),),)/SubjectAltName: (('DNS', 'aan.amazon.com'),)"]
+    f = open('certificates_line.txt', 'r')
+    # f = ["aan.amazon.com/Issuer: ((('countryName', 'US'),), (('organizationName', 'Amazon'),), (('commonName', 'Amazon RSA 2048 M01'),))/Subject: ((('commonName', 'aan.amazon.com'),),)/SubjectAltName: (('DNS', '*.taboola.com'), ('DNS', '*.taboolasyndication.com'), ('DNS', 'taboola.com'), ('DNS', 'taboolasyndication.com'))"]
     out = open(sys.argv[1], 'w')
-    print(f)
+    out_stats = open(sys.argv[2], 'w')
+    # print(f)
+    ca_counts = {}
     for line in f:
-        line = line.split('/')
-        w = line[0]
-        ca_line = line[1]
-        if 'ERROR' in ca_line:
-            if 'CERTIFICATE_VERIFY_FAILED' in ca_line:
-                out.write(f"{w} {'failed'}\n")
+        print(line)
+        if 'Error' in line:
+            if 'CERTIFICATE_VERIFY_FAILED' in line:
+                out.write(f"{'failed'}\n")
             else:
-                out.write(f"{w} {'address_error'}\n")
+                out.write(f"{'address_error'}\n")
             continue
-        ca_type = DEP_TYPE.UNKNOWN
-        host = ''
         line = line.split('/')
-        w = line[0]
-        ca_name = line[1].partition('organizationName,')[2].split(')')[0]
+        w = line[0].strip(':')
+        ca_line = line[1]
+        ca_type = DEP_TYPE.UNKNOWN
+        isHTTPS = True
+        host = ''
+        sub = line[3].split(':')[1].replace('(', '').replace(')', '').replace('\'', '').replace(' ', '')
+        SANs = [x for x in sub.split(',') if 'DNS' not in x]
+        ca_name = line[1].partition('organizationName\', ')[2].split(')')[0].strip('\'')
         print(w)
         print(ca_name)
+        print(SANs)
         
         
         # things we need:
@@ -38,34 +45,47 @@ def main():
         # -SOA(w)
         # -SOA(ca_url)
 
-        # w_ext = tld.extract(w)
-        # tld_w = ".".join([w_ext.domain, w_ext.suffix])
-        # ca_ext = tld.extract(ca)
-        # tld_ca = ".".join([ca_ext.domain, ca_ext.suffix])
-        # isHTTPS_w = isHTTPS(w)
-        # SAN_w = getSAN(w)
-        # SOA_w = getSOA(w)
-        # SOA_ca = getSOA(ca)
+        w_ext = tld.extract(w)
+        tld_w = ".".join([w_ext.domain, w_ext.suffix])
+        tld_ca = ''
+        if ca_name in ca_map.keys():
+            tld_ca = ca_map[ca_name]
+        if ca_name in ca_counts.keys():
+            ca_counts[ca_name] += 1
+        else:
+            ca_counts[ca_name] = 1
+        SOA_w = getSOA(w)
+        SOA_ca = getSOA(ca_name)
+        inSAN = [tld_ca in SAN for SAN in SANs]
+        print(inSAN)
 
-        # check if tld matches
-        # if tld_w == tld_ca:
-        #     ca_type = DEP_TYPE.PRIVATE
-        # # check if HTTPS and tld(ca) in SAN(w)
-        # elif (isHTTPS_w and tld_ca in SAN_w):
-        #     ca_type = DEP_TYPE.PRIVATE
-        # # check if SOA(ca) != SOA(w)
-        # elif (SOA_ca != SOA_w):
-        #     ca_type = DEP_TYPE.THIRD
+        #check if tld matches
+        if tld_w == tld_ca:
+            ca_type = DEP_TYPE.PRIVATE
+            print(f"matched {tld_w} and {tld_ca}")
+        # check if HTTPS and tld(ca) in SAN(w)
+        elif (isHTTPS and any(inSAN)):
+
+            ca_type = DEP_TYPE.PRIVATE
+        # check if SOA(ca) != SOA(w)
+        elif (SOA_ca != SOA_w):
+            ca_type = DEP_TYPE.THIRD
 
         
-        # if ca_type == DEP_TYPE.UNKNOWN:
-        #     final = 'UNKNOWN'
-        # elif ca_type == DEP_TYPE.PRIVATE:
-        #     final = 'PRIVATE'
-        # else:
-        #     final = 'THIRD'
+        if ca_type == DEP_TYPE.UNKNOWN:
+            final = 'UNKNOWN'
+        elif ca_type == DEP_TYPE.PRIVATE:
+            final = 'PRIVATE'
+        else:
+            final = 'THIRD'
 
-        # out.write(f"{w} {final} {host}\n")
+        out.write(f"{w}:{final}:{ca_name}\n")
+    for key, value in ca_counts.items():
+        out_stats.write(f"{key}:{value}\n")
+    out_stats.close()
     f.close()
     out.close()
         
+
+if __name__ == '__main__':
+    main()
---
+++ graphCA.py
@@ -0,0 +1,71 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import seaborn as sb
+import numpy as np
+import pandas as pd
+
+
+# print(nx.__version__)
+
+ca_map = {'Amazon': 'amazontrust.com', 'Google Trust Services LLC': 'pki.goog', 'Entrust, Inc.': 'entrust.com', 
+            'DigiCert Inc': 'digicert.com', 'Sectigo Limited': 'sectigo.com', 'COMODO CA Limited': 'comodoca.com', 'Microsoft Corporation': 'microsoft.com',
+            'Let\'s Encrypt': 'letsencrypt.org', 'Cloudflare, Inc.': 'cloudflare.com', 'GlobalSign nv-sa': 'globalsign.com', 'ZeroSSL': 'zerossl.com'}
+
+def digraphPlot():
+    f = open('ca_categorized.txt', 'r')
+    count = 0
+    labels = {}
+    DG = nx.DiGraph()
+    for line in f:
+        if 'failed' in line or 'address_error' in line:
+            continue
+        line = line.split(':')
+        trust = line[2].strip('\n')
+        DG.add_edge(f"{str(count)}", trust, weight = 1)
+        count += 1
+
+    for node in DG.nodes():
+        # print(node)
+        if node in ca_map.keys():
+            # print(node)
+            labels[node] = node
+
+    d = dict(DG.degree)
+
+    # values = [val_map.get(node, 0.25) for node in DG.nodes()]
+    # print(d)
+    
+    nx.draw(DG, nodelist=list(d.keys()), node_size=[v * 100 for v in d.values()], with_labels=False, cmap=plt.get_cmap('jet'))
+    pos = nx.drawing.nx_agraph.graphviz_layout(DG)
+    nx.draw_networkx_labels(DG, pos, labels, font_size=12, font_color='r')
+    
+    # pos = nx.spring_layout(DG)
+    # nx.draw_networkx_nodes(DG, pos, cmap=plt.get_cmap('jet'),
+    #                         node_color=values, node_size=500)
+    plt.show()
+
+
+def piePlot():
+    sb.set_theme(style="whitegrid", palette="pastel")
+    data = []
+    keys = []
+    f = open('ca_stats.txt', 'r')
+    for line in f:
+        line = line.split(":")
+        val = line[1].strip('\n')
+        data.append(val)
+        if int(val) > 20:
+            keys.append(line[0])
+        else:
+            keys.append('')
+
+    # df = pd.DataFrame({'Category': data})
+    # sizes = df['Category'].sort_values()
+
+    plt.pie(data, labels=keys)
+    # plt.legend(loc=3, labels=keys)
+    # plot = plt.get_figure()
+    plt.savefig('CA_pie', bbox_inches='tight')
+
+if __name__ == "__main__":
+    piePlot()
